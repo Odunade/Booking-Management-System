33346fb5fc47d109d3a10054b3e04821d5c61ab0
ODUNADE AHMED <88735479+Odunade@users.noreply.github.com>
2022-10-14 16:18:51 +0100

    commit

--- src/compnents/navbar/Navbar.jsx
+++ src/compnents/navbar/Navbar.jsx
@@ -1,30 +1,17 @@
-.mail{
-  width: 100%;
-  margin-top: 50px;
-  background-color: #003580;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  padding: 50px;
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">lamabooking</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-.mailInputContainer>input{
-  width: 300px;
-  height: 30px;
-  padding: 10px;
-  border: none;
-  margin-right: 10px;
-  border-radius: 5px;
-}
-
-.mailInputContainer>button{
-  height: 50px;
-  background-color: #0071c2;
-  color: white;
-  font-weight: 500;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
+export default Navbar
